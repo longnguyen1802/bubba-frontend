ff66f2256d9212b986e28b5ec30ffbfe9204f783
longtune1802 <longnguyen@secrettune.io>
2022-08-15 12:45:16 +0800

    add image detail

--- src/pages/ImageDetail.js
+++ src/pages/ImageDetail.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
+import { Image } from 'cloudinary-react';
 import { IconButton } from '@mui/material';
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
-import { Link } from 'react-router-dom';
-
+import { Link ,useLocation} from 'react-router-dom';
+import axios from 'axios';
+import { URL } from '../components/constant';
 export default function ImageDetail() {
+  const [photographer,setPhotographer] = useState();
+  const location = useLocation();
+  const albumId = location.pathname.split("/")[4];
+  const imageId = location.pathname.split("/")[5];
+  useEffect(()=>{
+    const loadPhotograher = async () => {
+      const res = await axios.get(URL+'/api/album/getphotographer',{
+        mode:'cors',
+        params:{
+            albumId: albumId
+        }
+    });
+    const data = await res.data;
+    setPhotographer(data.photographer);
+    }
+    loadPhotograher();
+  });
+  
   return (
     <>
       <div className='homepage-container'>
@@ -11,10 +31,16 @@ export default function ImageDetail() {
           <IconButton>
             <Link to='/search/result'><ArrowBackIosIcon /></Link>
           </IconButton>
-          <h2>Taken by XXX</h2>
+          {<h2>Taken by {photographer}</h2>}
         </div>
         <div className='image-container'>
-
+          <Image
+              cloudName={process.env.REACT_APP_CLOUDINARY_NAME||"dr4xcuczn"}
+              publicId={"bubba_test/"+albumId+'/'+imageId}
+              width="300"
+              height="300"
+              crop="scale"
+          />
         </div>
         <div className='face-search-box'>
           <span>Click and search the face:</span>
