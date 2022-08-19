a0be5d0472a0ca8205f11f93c824a22cf6e30250
longtune1802 <longnguyen@secrettune.io>
2022-08-19 13:58:21 +0800

    finish front end

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,19 +10,11 @@ import ListAlbum from '../components/album/ListAlbum';
 export default function Home() {
     const {listAlbumThumbnail,listAllAlbum,listAlbumInfo,listAlbumDate} = useAPI()
     const eventList = [
-<<<<<<< HEAD
-        "Community Connect Programme",
-        "GS1 Event",
-        "Investors Matching Programme",
-        "Jumpstarter 2022",
-        "Road To GBA Start-Up Programme 2.0 Launch Event",
-=======
         "GS1 Event",
         "Community Connect Programme",
         "Road To GBA Start-Up Programme 2.0 Launch Event",
         "Investors Matching Programme",
         "Jumpstarter 2022",
->>>>>>> front-end-dev
         "Visit Of Shenzhen Hong Kong Macau Women Directors Alliance"
     ]
     const navigate = useNavigate();
@@ -43,139 +35,6 @@ export default function Home() {
             <Link to='/search' className='search-bar'> 
               <i className="material-icons">search</i>
               <input className='search-field' type="text" placeholder="Search here"></input>
-<<<<<<< HEAD
-            </Link> */}
-            <div className='event-filter-container'>
-              <div className={listEvent[0]?'event-box-after':'event-box'} id = "box-0" onClick={()=>{
-                handleFilterClick(0)
-              }}>
-                <span>Community Connect 
-                    <br/>Programme </span>
-              </div>
-              <div className={listEvent[1]?'event-box-after':'event-box'} id = "box-1" onClick={async ()=> {
-                handleFilterClick(1);
-              }}>
-                <span>GS1 Event</span>
-              </div>
-              <div className={listEvent[2]?'event-box-after':'event-box'} id = "box-2" onClick={()=>{
-                handleFilterClick(2)
-              }}>
-                <span>Investors Matching <br/> Programme</span>
-              </div>
-              <div className={listEvent[3]?'event-box-after':'event-box'} id = "box-3" onClick={()=>{
-                handleFilterClick(3)
-              }}>
-                <span>Jumpstarter 2022</span>
-              </div>
-              <div className={listEvent[4]?'event-box-after':'event-box'} id = "box-4" onClick={()=>{
-                handleFilterClick(4)
-              }}>
-                <span>Road To GBA <br/>Start-Up Programme 2.0 <br/>Launch Event</span>
-              </div>
-              <div className={listEvent[5]?'event-box-after':'event-box'} id = "box-4" onClick={()=>{
-                handleFilterClick(5)
-              }}>
-                <span>Visit Of Shenzhen <br/> Hong Kong Macau Women <br/> Directors Alliance</span>
-              </div>
-            </div>
-              
-            {
-                listEvent.filter(Boolean).length === 0
-                ?
-                (
-                    <div className='album-container'>
-                        <h2>What's New</h2>
-                        { 
-                        listAlbumThumbnail.length>0 
-                        && 
-                        listAlbumThumbnail.map((value,index)=>(
-                            <div className='album' onClick={()=>{
-                                navigateAlbum(listAllAlbum[index])
-                            }}>
-                                <Image
-                                    key={index}
-                                    cloudName={process.env.REACT_APP_CLOUDINARY_NAME||"dfrouqxub"}
-                                    publicId={value}
-                                    width="400"
-                                    height="200"
-                                    crop="scale"
-                                />
-                                {/* <div className='album-info' id = {"album-"+index}>
-                                    <p>Album title</p>
-                                    <span>Created in yyyy/mm/dd</span>
-                                </div> */}
-                            </div>
-                        ))   
-                        }
-                    </div>
-                )
-                :
-                (
-                    <div>
-                        <FacereCognition listFolder={listAlbum}/>
-                        <br></br>
-                        <h1>Preview of all filter image</h1>
-                        <h2>There is current {currentImageId&& currentImageId.length} images</h2>
-                        <div className="gallery">
-                        {
-                            currentImageId && currentImageId.map((imageId,index)=>(
-                                    <Image
-                                    key={index}
-                                    cloudName={process.env.REACT_APP_CLOUDINARY_NAME||"dfrouqxub"}
-                                    publicId={imageId}
-                                    width="300"
-                                    height="300"
-                                    crop="scale"
-                                />
-                            ))
-                        }
-                        </div>
-                    </div>
-                )
-            }
-            
-            {/* <h2>There is current {currentImageId&& currentImageId.length} images</h2>
-            <br></br>
-            <p> You have {credit} free credit</p>
-            <br></br>
-            <label>Photographer</label>
-            <input
-                className="search"
-                onChange={(e) => setPhotographer(e.target.value.toLowerCase())}
-            />
-            <br></br>
-            <label>Date</label>
-            <input
-                className="search"
-                onChange={(e) => setDate(e.target.value.toLowerCase())}
-            />
-            <br></br>
-            <label>Event</label>
-            <input
-                className="search"
-                onChange={(e) => setEvent(e.target.value.toLowerCase())}
-            />
-            <br></br>
-            <button onClick={handleFilter}>Filter</button> 
-            <br></br>
-            
-            <FacereCognition listFolder ={listAlbum}/>
-            <br></br>
-            <h1>Preview of all image</h1>
-            <div className="gallery">
-            {
-                currentImageId && currentImageId.map((imageId,index)=>(
-                        <Image
-                        key={index}
-                        cloudName={process.env.REACT_APP_CLOUDINARY_NAME||"dfrouqxub"}
-                        publicId={imageId}
-                        width="300"
-                        crop="scale"
-                    />
-                ))
-            }
-            </div> */}
-=======
             </Link>
             <h3>Event Album</h3>
             <FormControl fullWidth>
@@ -206,7 +65,6 @@ export default function Home() {
                            navigateAlbum={navigateAlbum} listAlbumDate={actualDate} 
                            listAlbumInfo={actualInfo}/>
             </>     
->>>>>>> front-end-dev
         </div>
     );
 }                        
