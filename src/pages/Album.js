ce50af3cab858f76a9404087736341d2b1d9cdec
longtune1802 <longnguyen@secrettune.io>
2022-08-19 14:31:45 +0800

    fix album

--- src/pages/Album.js
+++ src/pages/Album.js
@@ -1,10 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Image } from 'cloudinary-react';
-import axios from 'axios';
 import { useLocation } from 'react-router-dom';
-import {URL} from '../components/util/constant.js';
-
-import AccountBoxOutlinedIcon from '@mui/icons-material/AccountBoxOutlined';
 import AlertModal from '../components/notify/AlertModal';
 import { Link } from 'react-router-dom';
 import { IconButton } from '@mui/material';
@@ -13,44 +8,13 @@ import FaceFilterBox from '../components/search/face/FaceFilterBox.js';
 import ImageCard from '../components/image/ImageCard.js';
 import { useAPI } from '../context/dataContext.js';
 export default function Album() {
-    const [imageIds, setImageIds] = useState();
     const location = useLocation();
     const id = location.pathname.split("/")[2];
     const[AlertIsOpen, setAlertIsOpen] = useState(false)
-    const {setListAlbum} = useAPI();
-    const loadData = async () => {
-        try {
-            const res = await axios.get(URL+'/api/image/album',
-                {
-                    mode:'cors',
-                    params:{
-                        albumId:id
-                    }
-                }
-            );
-            const data = await res.data;
-            setListAlbum([id]);
-            setImageIds(data);
-        } catch (err) {
-            try{
-                const res = await axios.get(URL+'/api/image/album',
-                {
-                    mode:'cors',
-                    params:{
-                        albumId:id
-                    }
-                }
-            );
-                const data = await res.data;
-                setImageIds(data);
-            }
-            catch(err){
-                console.error(err);
-            }   
-        } 
-    };
+    const {imageIds,setListAlbum} = useAPI();
+    const listImage = imageIds.filter((value)=>(value.folder===id)).map(e=>e.files).flat();
     useEffect(() => {
-        loadData();
+        setListAlbum([id]);
     }, []);
     return (
         <div>
@@ -62,15 +26,8 @@ export default function Album() {
                 <h2>Album {id}</h2>
               </div>
               <div className='result-image-container'>
-                {imageIds &&
-                  imageIds.map((imageId, index) => (
-                      // <Image
-                      //     key={index}
-                      //     cloudName={process.env.REACT_APP_CLOUDINARY_NAME||"dr4xcuczn"}
-                      //     publicId={imageId}
-                      //     crop="scale"
-                      //     className='image-box'
-                      // />
+                {listImage &&
+                  listImage.map((imageId, index) => (
                       <ImageCard key={index} publicId={imageId} />
                   ))}
               </div>
