c6ea1efa2373ba44855023c631b58ebe289ece90
longtune1802 <longnguyen@secrettune.io>
2022-08-29 10:22:38 +0800

    final

--- src/components/album/ListAlbum.js
+++ src/components/album/ListAlbum.js
@@ -9,7 +9,9 @@ import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
 import { convertDate } from '../../util/filter/convert';
+import { PREFIX_URL } from '../util/constant';
 const ListAlbum = ({listAlbumThumbnail,listAllAlbum,navigateAlbum,listAlbumInfo,listAlbumDate}) => {
+  //console.log(listAlbumThumbnail,listAlbumInfo)
   return (
     <div className='album-container'>      
         {   
@@ -25,7 +27,8 @@ const ListAlbum = ({listAlbumThumbnail,listAllAlbum,navigateAlbum,listAlbumInfo,
             }}>
                 <Card sx={{ maxWidth: "388px", borderRadius: "20px", marginTop: "20px"}}>
                 <CardActionArea>
-                    <Image
+                    <img src={PREFIX_URL+value} alt="Default Image" width="388" height="233"/>
+                    {/* <Image
                         key={index}
                         cloudName={process.env.REACT_APP_CLOUDINARY_NAME||"dr4xcuczn"}
                         publicId={value}
@@ -33,7 +36,7 @@ const ListAlbum = ({listAlbumThumbnail,listAllAlbum,navigateAlbum,listAlbumInfo,
                         width="388"
                         height="233"
                         crop="scale"
-                    />
+                    /> */}
                     <CardContent className='album-info'>
                     <Typography gutterBottom component="div">
                         {listAlbumInfo[index]}
--- src/components/image/ImageCard.js
+++ src/components/image/ImageCard.js
@@ -3,14 +3,14 @@ import Card from '@mui/material/Card';
 import { CardActionArea } from '@mui/material';
 import { Image } from 'cloudinary-react';
 import {useNavigate} from 'react-router-dom';
-
+import { PREFIX_URL } from '../util/constant';
 
 export default function ImageCard({key, publicId}) {
   const navigate = useNavigate();
   const navigateImage = (publicId) =>{
     const splitId = publicId.split('/');
-    const albumId = splitId[1];
-    const imageId = splitId[2];
+    const albumId = splitId[0];
+    const imageId = splitId[1];
     navigate('/search/result/image/'+albumId+'/'+imageId)
 }
   return (
@@ -18,14 +18,15 @@ export default function ImageCard({key, publicId}) {
       <CardActionArea onClick={() => {
         navigateImage(publicId);
       }} >
-        <Image
+        <img src={PREFIX_URL+publicId} alt="Default image" width="200" height="200"/>
+        {/* <Image
           key={key}
           cloudName={process.env.REACT_APP_CLOUDINARY_NAME||"dr4xcuczn"}
           publicId={publicId}
           crop="scale"
           style={{ aspectRatio: 3/2 }}
           className='image-box'
-        />
+        /> */}
       </CardActionArea>
     </Card>
 
